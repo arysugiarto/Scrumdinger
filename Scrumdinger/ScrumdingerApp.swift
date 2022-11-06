b4b0eaaa1d4d6253f0e344ef9868142d943f324c
arysugiarto <arysugiarto10@gmail.com>
2022-11-06 22:23:03 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ary Sugiarto on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
