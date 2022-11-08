f2fad9afc8b184996f69370681007baa76e714d8
arysugiarto <arysugiarto10@gmail.com>
2022-11-08 17:15:58 +0700

    Add edit data

--- Scrumdinger/ui/DetailView.swift
+++ Scrumdinger/ui/DetailView.swift
@@ -7,25 +7,27 @@
 
 import SwiftUI
 
-struct DetailView : View{
+struct DetailView: View {
     let scrum: DailyScrum
     
-    var body: some View{
-        List{
+    @State private var isPresentingEditView = false
+    
+    var body: some View {
+        List {
             Section(header: Text("Meeting Info")) {
-                NavigationLink(destination: MeetingView()){
+                NavigationLink(destination: MeetingView()) {
                     Label("Start Meeting", systemImage: "timer")
                         .font(.headline)
                         .foregroundColor(.accentColor)
                 }
-                HStack{
-                    Label("Lengt",systemImage: "clock")
+                HStack {
+                    Label("Length", systemImage: "clock")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
                 .accessibilityElement(children: .combine)
-                HStack{
-                    Label("Theme", systemImage:"paintpalette")
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
                     Spacer()
                     Text(scrum.theme.rawValue)
                         .padding(4)
@@ -35,19 +37,37 @@ struct DetailView : View{
                 }
                 .accessibilityElement(children: .combine)
             }
-            Section(header: Text("Atendees")) {
-                ForEach(scrum.attendees){
-                    attendee in
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
                     Label(attendee.name, systemImage: "person")
                 }
+            }
+        }
+        .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
             }
         }
     }
 }
 
-struct DetailView_Previews: PreviewProvider{
-    static var previews: some View{
-        NavigationView{
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
             DetailView(scrum: DailyScrum.sampleData[0])
         }
     }
